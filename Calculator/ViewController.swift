ee0122fdbbb0cac6c2abde6d69d56de8cb5dc507
Stefanenko Leonid <stefanenko_lu@hotmail.com>
2021-01-01 17:07:14 +0300

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Leonid Stefanenko on 01.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
